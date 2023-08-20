9190bc8593c7173ed37e1d7942d284d754273df7
hectoremiliano <emiliano.locke16@gmail.com>
2023-08-19 20:15:41 -0600

    list

--- README.md
+++ README.md
@@ -3,12 +3,8 @@
 - Nombre: Hector Emiliano Custodio Martinez
 - Correo: emiliano.locke16@gmail.com
 
-## Actividad 1
-- programa 1 
-- programa 2
-
-## Actividad 2
-- programa 3
-- programa 4
-## Actividad 3
-- programa 5
+## Clase 1
+_p01_HolaMundo
+_p02_AreaCirculo
+_p03_AreaTriangulo
+_p04_PagaTrabajador
---
+++ _p01_HolaMundo.java
@@ -0,0 +1,14 @@
+// Hola Hector
+
+public class _p01_HolaMundo {
+   public static void main(String[] args) {
+     String amigo = "Diego";
+     String hermano = "Alejandra";
+     String mensaje = String.format("Tanto %s como %s desean aprender a programar en java", amigo, hermano);
+    
+    System.out.println("Hola Mundo en el lenguaje Java");
+System.out.println("\nHola amigo "  + amigo + " bienvenido a java" );
+    System.out.println("\nMi amigo es " + amigo + " y mi hermano es " + hermano + "\n");
+    System.out.println(mensaje);
+   }
+}
